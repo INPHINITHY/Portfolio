85cb80a9c80dc51fd8ee7385b3e1fed8b043ce13
Austin <dwomohaustin14@gmail.com>
2024-09-25 16:44:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link href=".\..\css\customStyleSheet.css" rel="stylesheet" type="text/css" />
-<title>About Us</title>
-</head>
   <title>Welcome</title>
 </head>
 <body>
