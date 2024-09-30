46982aa461a00d42241cf0ed8c873bc074afd68b
Austin <dwomohaustin14@gmail.com>
2024-09-29 19:57:52 -0500

    remasker

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,11 @@
 <nav id="nav">
- <ul style="margin:0">
+<a href="#nav" title="Show navigation"><img src="images/hamburger.png" valign="middle" alt="menu icon" /> Menu</a>
+<a href="#" title="Hide navigation"><img src="images/hamburger.png" valign="middle" alt="menu icon" /> Menu</a>
+ <ul>
   <li><a href="home.htm" >Home</a></li>
   <li><a href="about.htm">About</a></li>
   <li><a href="Skills.htm">Skills</a></li>
+  <li><a href="Projects.htm">Projects</a></li>
  </ul>
     <script>
         $( function()
--- index.php
+++ index.php
@@ -5,12 +5,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="doubletaptogo.min.js" type="text/javascript"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> -->
-<link href="css\customStyleSheet.css" rel="stylesheet" type="text/css" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
 <title>AUSTIN N. DWOMOH</title>
 </head>
 <body>
@@ -19,73 +17,44 @@
   </header>
   
 <!--Main Content Start-->
-<div class="container">
-  <div class="grid-two-columns">
+
+  <div class="container">
+  <div class="grid-two-columns" style=" ">
     <div>
-        <h2>Hi, I am Austin N. Dwomoh</h2>
+      <span id="hero-text" >
+        <h1>Hi, I am Austin N. Dwomoh</h1>
         <p>A Computer Science Major, At Augustana College, With A Love and Knack For Tecnology.</p>
+      </span>
+      <span style="margin:auto;">
+          <button class="btn">Resume</button>
+          <button class="btn-hol">Projects </button>
+        </span>
     </div>
-    <div>
-        <img src="assets\1st.jpg" alt="">
+    <div >
+        <img  class="card-img" src="assets\1st.jpg" alt="">
     </div>
   </div>
 </div>
+  <div class="container">
+    <div class="grid-two-columns">
+      <div>
+          <img class="card-img" src="assets\2nd.jpg" alt=""/>
+      </div>
 
-    <!--Main Start-->
-    <div class="container">
-      <p class="h1" style="text-align: center">WELCOME</p>
-      <p class="lead" style="text-align: center">To My Portfolio</p>
-      <div class="row">
-        <div class="col">
-          <img class="img-fluid" src="../assets/2nd.jpg" alt=""/>
+        <div>
+          <div class="card">
+          <h3>A little About ME</h3>
+          <ul>
+            <li>Name:Austin Dwomoh</li>
+            <li>Age:20</li>
+            <li><a href="mailto:dwomohaustin14@gmail.com" style="text-decoration: none; color: beige">Personal Mail</a></li>
+            <li><a href="tel:+13096314966"style="text-decoration: none; color: beige">Phone</a></li>
+          </ul>
         </div>
-
-        <div class="col" style="margin: auto">
-          <table class="table table-borderless card">
-            <thead>
-              <tr>
-                <p
-                  class="h2 text-warning"
-                  style="margin-bottom: 20px; text-align: center">
-                  Classics
-                </p>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>Name:</th>
-                <td>Austin Dwomoh</td>
-              </tr>
-              <tr>
-                <th>Age:</th>
-                <td>20</td>
-              </tr>
-              <tr>
-                <th>Email :</th>
-                <td>
-                  <a
-                    href="mailto:dwomohaustin14@gmail.com"
-                    style="text-decoration: none; color: beige"
-                    >dwomohaustin14@gmail.com</a
-                  >
-                </td>
-              </tr>
-              <tr>
-                <th>Phone:</th>
-                <td>
-                  <a
-                    href="tel:+13096314966"
-                    style="text-decoration: none; color: beige"
-                    >(309) 631 4966</a
-                  >
-                </td>
-              </tr>
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
-    <!--links-->
+    
     <div class="container">
       <div class="row">
         <div class="col mt-5">
@@ -99,6 +68,10 @@
               alt=""
           /></a>
         </div>
+
+          </span>
+
+
         <div class="col mt-5">
           <p class="h4 text-warning hoverText" data-hover="Skills">Skills</p>
           <a href="./Skills.htm">
@@ -106,7 +79,7 @@
           /></a>
         </div>
       </div>
-    </div>
+    </div> 
 </body>
 
 <footer>
